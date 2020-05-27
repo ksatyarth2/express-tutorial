55fd2b154a4293f2eb3a59224c26bbfa2676b1b8
ksatyarth2 <erkumar@protonmail.ch>
2020-05-27 16:57:38 +0530

    Working Forms with express

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');    //using body-parser module
+
+var app = express();                        //using express module
+
+var urlencodedParser = bodyParser.urlencoded({ extended: true })      //used body-parser for forms parsing
 
 
-var server = http.createServer(function(req, res){
-    console.log("request was made at:"+ req.url);
-    if(req.url === '/home' || req.url === '/'){
-        res.writeHead(200,{'Content-Type':'text/html'});
-        console.log("Request generated");
-        fs.createReadStream(__dirname+'/index.html').pipe(res);
-    }    
-    else if (req.url === '/about') {
-        res.writeHead(200,{'content-type':'text/html'});
-        fs.createReadStream(__dirname+"/about.html").pipe(res);
-    }
-    else{
-        res.writeHead(404,{'Content-Type':'text/html'});
-        fs.createReadStream(__dirname+'/404.html').pipe(res);
-    }
+app.set('view engine', 'ejs');                  //template engine ejs
+app.use('/assets',express.static('assets')); //middleware
+
+app.get('/', function (req, res) {
+    res.render('index');                    //render is used for returning template ejs
 });
-server.listen(3000,'127.0.0.1');
-    
+app.get('/about', function (req, res) {
+    // console.log(req.query);
+    res.render('about',{qs:req.query});
+});
+app.post('/about', urlencodedParser, function (req, res) {      //using post method we are parsing the forms details in this middleware
+    console.log(req.body);                                      //using body to print forms details
+    res.render('about-success',{data: req.body});               //passing the data from about page to about-success
+                                                                //use node mailer to use mail function.read more
+});
+
+app.get('/profile/:name', function (req, res) {
+    var data = {Age:12, Hobbies:['Coding', 'sleeping','eating']}
+    res.render('profile', { person: req.params.name, data: data });  //passing methods in views
+});
+
+app.listen(3000);
